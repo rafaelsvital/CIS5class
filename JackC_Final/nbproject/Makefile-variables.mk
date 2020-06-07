48bb2cf1267b377fbff359ba8ddca33a12bbf0dd
rafaelsvital <61526402+rafaelsvital@users.noreply.github.com>
2020-06-07 03:05:16 -0700

    Add files via upload

---
+++ JackC_Final/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=jackc_final
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/jackc_final
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=jackcfinal.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/jackcfinal.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=jackc_final
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/jackc_final
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=jackcfinal.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/jackcfinal.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
